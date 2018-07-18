625ca31cb02e6e1236dcea4ed08184f0d4b733f1
tsaaii <stsurapa@uwaterloo.ca>
2018-07-18 01:12:39 -0400

    added case 1

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,15 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.webhook = functions.https.onRequest((request, response) => {
-    
-        console.log("request.body.result.parameters: ", request.body.result.parameters);
-        // {
-        //     name: "john",
-        //     persons: "3"
-        // }
-    
-        let params = request.body.result.parameters;
-    
+
+    switch (request.body.result.action) {
+        case 'joe':
         response.send({
             speech:
-                `${params.name} your hotel booking request for ${params.RoomType} room is forwarded for ${params.persons} persons, we will contact you on ${params.email} soon`
+                `Hi Nanna `
         });
+
+        default;
+    }
+    
     });
