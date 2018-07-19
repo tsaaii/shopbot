56c5f03265f1a5cde57162ca97939def696214e9
tsaaii <stsurapa@uwaterloo.ca>
2018-07-19 11:56:58 -0400

    added test query

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,23 @@
 const functions = require('firebase-functions');
 exports.webhook = functions.https.onRequest((request, response) => {
     switch (request.body.result.action) {
-        case 'joe':
-        response.send({
-            speech:
-                `Hi Nanna `
-        });
-        break;
-        case 'bujji':
-        response.send({
-            speech:
-                `apakshampak kirirkiri `
-        });
-        break;
-        default:
-            response.send({
-            speech:
-                `edo thappu jariginde `
+
+        case 'db':{
+            var apples = db.collection('apple');
+            var allNames = apples.get()
+                .then(snapshot => {
+                    snapshot.forEach(doc => {
+                        console.log(doc.id, '=>', doc.data());
+              });
+             
+            })
+            .catch(err => {
+              console.log('Error getting products', err);
             });
+            response.send({
+                speech:{$allNames}
+            });   
+        }
+        break;
     }
     });
