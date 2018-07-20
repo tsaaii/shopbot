453cfbf0a2d6d6f5a9ae085a34f5a8188c96b996
tsaaii <stsurapa@uwaterloo.ca>
2018-07-19 20:59:58 -0400

    querying on voice

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,31 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+var firestore = admin.firestore();
+
+
 exports.webhook = functions.https.onRequest((request, response) => {
     switch (request.body.result.action) {
 
-        case 'db':{
-            var apples = db.collection('apple');
-            var allNames = apples.get()
-                .then(snapshot => {
-                    snapshot.forEach(doc => {
-                        console.log(doc.id, '=>', doc.data());
+        case 'joe':{
+
+            firestore.collection('zehrs_db').add(request.body.fulfillment.speech)
+                .then((querySnapshot)=> {
+                    response.send({
+                        speech: 
+                        '${request.body.fulfillment.speech} is added to the db'
               });
-             
+             return[response]
             })
-            .catch(err => {
-              console.log('Error getting products', err);
+            .catch((e => {
+              console.log('Error getting products', e);
+              response.send({
+                speech:'${allNames}'
             });
-            response.send({
-                speech:{$allNames}
-            });   
+            
+            }))  
+            break;
         }
-        break;
     }
-    });
+});
